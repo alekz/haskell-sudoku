675b229be51a0a712758e2e9a9b27665906c3e0a
Alexander Konstantinov <konstantinov@gmail.com>
2010-07-26 22:38:02 +0300

    Introduced custom data types

--- src/Sudoku.hs
+++ src/Sudoku.hs
@@ -2,21 +2,33 @@ module Sudoku where
 
 import Data.List ((\\), nub)
 
+type Coord = Int  -- 1..9
+type CellCoord = (Coord, Coord)
+type Cell = Int  -- 0..9
+type Board = [[Cell]]
+
+cells :: [CellCoord]
 cells = [(x,y) | x <- [1..9], y <- [1..9]]
 
+row :: CellCoord -> [CellCoord]
 row (x, y) = [(x', y) | x' <- ([1..9] \\ [x])]
 
+col :: CellCoord -> [CellCoord]
 col (x, y) = [(x, y') | y' <- ([1..9] \\ [y])]
 
+group :: CellCoord -> [CellCoord]
 group (x, y) = [(x', y') | x' <- groupCoords x, y' <- groupCoords y] \\ [(x, y)]
     where firstCoord n = 3 * ((n - 1) `div` 3) + 1
           groupCoords n = take 3 [(firstCoord n)..]
 
-relatedCells cell = nub $ (row cell) ++ (col cell) ++ (group cell)
+relatedCells :: CellCoord -> [CellCoord]
+relatedCells coord = nub $ (row coord) ++ (col coord) ++ (group coord)
 
+boardCell :: Board -> CellCoord -> Cell
 boardCell board (x,y) = board !! (y - 1) !! (x - 1)
 
-relatedCellsValues board cell = nub [boardCell board c | c <- relatedCells cell] \\ [0]
+relatedCellsValues :: Board -> CellCoord -> [Cell]
+relatedCellsValues board coord = nub [boardCell board c | c <- relatedCells coord] \\ [0]
 
 {-
 replaceCell board cell value = ...
